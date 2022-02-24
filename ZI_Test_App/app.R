e1b91c0af6e30d652140ff654ae204b467ca22c6
MartinLecocq <Martinl@hotmail.be>
2022-02-24 11:34:49 +0000

    Début de la tentative d'ajout de la préparation du train set, et du tri. Prise de connaissance de méthodes utilisables.

--- ZI_Test_App/app.R
+++ ZI_Test_App/app.R
@@ -17,8 +17,6 @@ setwd("/home/rstudio/shared/zooimage-ui/ZI_Test_App/")
 smpfiles <- list.files("www/Samples/")
 smps <- smpfiles[!grepl("Description.zis", smpfiles)]
 smps <- smps[!grepl(".zidb", smps)]
-smps_with_path <- paste0("www/Samples/",smps)
-# zidb_files <- smpfiles[grep(".zidb", smpfiles)]
 
 old_stringsAsFactors <- getOption("stringsAsFactors")
 options(stringsAsFactors = TRUE)
@@ -40,6 +38,7 @@ ui <- fluidPage(
                 sidebarPanel(
                     selectInput("sample_folder", "Sample folder :", choices = smps),
                     actionButton("zidbmake", "Make the ZIDB file"),
+                    tags$br(),
                     tags$br(),
                     
                     actionButton("zidbmakeall", "Make ZIDB file for all of the samples"),
@@ -91,7 +90,32 @@ ui <- fluidPage(
             )
         ),
         tabPanel("Data Preprocessing",
-            
+            # ConditionalPanel permettant de faire en sorte que cette UI
+            # ne s'affiche que si on a des fichiers ZIDB
+            conditionalPanel(
+                condition = "output.zidb_files > 0",
+                
+                tabsetPanel(
+                    
+                    # Page de préparation du Train Set
+                    tabPanel("Preparing Train Set",
+                             
+                        fluidRow(
+                            column(width = 12,
+                                tags$h3("Files for Train Set") ,
+                                checkboxGroupInput("zidb_to_prepare", label = "" ,choices = smpfiles[grepl(".zidb", smpfiles)]),
+                                selectInput("preptrain_template", "Template :", choices = c("[Detailed]", "[Basic]", "[Very detailed]")),
+                                actionButton("prepare", "Prepare Train Set"),
+                            )
+                        )
+                    ),
+                    
+                    # Page de tri
+                    tabPanel("Manual Sorting",
+                        
+                    )
+                )
+            )
         ),
     )
 )
@@ -137,6 +161,7 @@ server <- function(input, output, session) {
             "Select a ZIDB file to show a preview :", choices = zidb_files())
         updateSelectInput(session, "vignettes_file",
             "ZIDB file to visualize", choices = zidb_files())
+        updateCheckboxGroupInput(session, "zidb_to_prepare", "", choices = zidb_files())
     })
     
     
@@ -147,6 +172,7 @@ server <- function(input, output, session) {
             "Select a ZIDB file to show a preview :", choices = zidb_files())
         updateSelectInput(session, "vignettes_file",
             "ZIDB file to visualize", choices = zidb_files())
+        updateCheckboxGroupInput(session, "zidb_to_prepare", "", choices = zidb_files())
     })
     
     
@@ -269,6 +295,19 @@ server <- function(input, output, session) {
 # ============ Deuxième page ============
     
     
+    # output pour créer un panneau conditionnel dans mon UI
+    # (apparait que si des fichiers ZIDB sont présents)
+    output$zidb_files <- reactive({
+        input$zidbmake
+        input$zidbmakeall
+        smpfiles <- list.files("www/Samples/")
+        length(smpfiles[grepl(".zidb", smpfiles)])
+    })
+    
+    
+    # Nécessaire pour le que browser charge la valeur la plus récente
+    # d'output pour évaluer correctement la conditon
+    outputOptions(output, "zidb_files", suspendWhenHidden = FALSE)
 }
 
 
