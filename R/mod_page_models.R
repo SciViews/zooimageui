51503d17d2d9b7ee6692c368ccc8abce35c3f8b9
MartinLecocq <Martinl@hotmail.be>
2022-04-08 15:03:52 +0000

    J'ai commencé à mettre en place ma page Models. J'ai mis en place un sélecteur de script. Un affichage des scripts existants, et égalements des boutons pour rafraichir ou utiliser le script. J'ai créé différentes variables pour avoir par exemple le chemin du dossier, ou encore la liste des scripts. J'ai réalisé un test avec un bête script qui met en place une variable result qui retourne "Bravo...". Je l'ai utilisé dans mon interface et cela a fonctionné ! Je pense être proche de savoir comment utiliser un script pour faire un classifieur.

--- R/mod_page_models.R
+++ R/mod_page_models.R
@@ -10,7 +10,45 @@
 mod_page_models_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    tabsetPanel(
+
+# Create Classifier UI ----------------------------------------------------
+
+      tabPanel("Create Classifier",
+        tags$br(),
+        sidebarLayout(
+          
+          sidebarPanel(
+            h4("Chose the Script to use :"),
+            selectInput(ns("modcre_selected_script"), NULL, choices = NULL),
+            shinyjs::disabled(actionButton(ns("modcre_use_selected_script"), "Use Script")),
+          ),
+          
+          mainPanel(
+            h4("Existing Scripts :"),
+            actionButton(ns("modcre_refresh"), "Refresh Scripts List"),
+            tags$br(),
+            tags$br(),
+            verbatimTextOutput(ns("modcre_existing_script_show")),
+            verbatimTextOutput(ns("modcre_test")),
+          ),
+          
+        ),
+      ),
+      
+# Test Classifier UI ------------------------------------------------------
+      
+      tabPanel("Test Classifier",
+        
+      ),
+      
+# Visualise Classifier UI -------------------------------------------------
+      
+      tabPanel("Visualise Classifier",
+        
+      ),
+      
+    )
   )
 }
     
@@ -20,7 +58,82 @@ mod_page_models_ui <- function(id){
 mod_page_models_server <- function(id, all_vars){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+    
+# Récupération Des Variables -----------------------------------------------
+    
+    # Settings Vars :
+    data_folder_path_rea <- reactive({ all_vars$settings_vars$data_folder_path_rea })
+    
+    
+# Variables Globales ------------------------------------------------------
+    
+    # Variable : Chemin vers le dossier des scripts models
+    models_folder_path <- reactive({
+      fs::path(data_folder_path_rea(),"Models")
+    })
+    
+    # Variable : liste des scripts dans Models
+    scripts_list <- reactive({
+      input$modcre_refresh
+      list.files(models_folder_path())
+    })
+    
+# Create Classifier Server ------------------------------------------------
+    
+    # Mise à jour du sélecteur de scripts models
+    observe({
+      # Si il y a des scripts :
+      if (length(scripts_list()) > 0) {
+        updateSelectInput(session, "modcre_selected_script", NULL, choices = scripts_list())
+      # Si pas :
+      } else {
+        updateSelectInput(session, "modcre_selected_script", NULL, choices = "No Script yet")
+      }
+    })
+    
+    # Mise à jour du bouton pour utiliser un script models
+    observe({
+      shinyjs::disable("modcre_use_selected_script")
+      if (req(input$modcre_selected_script) != "No Script yet") {
+        shinyjs::enable("modcre_use_selected_script")
+      }
+    })
+    
+    # Affichage // Liste des scipts dans le dossier Models
+    output$modcre_existing_script_show <- renderPrint({
+      if (length(scripts_list()) > 0) {
+        scripts_list()
+      } else {
+        "No Script yet"
+      }
+    })
+    
+    # Variable : Si on utilise le script, retourne la variable result de ce dernier
+    test <- eventReactive(input$modcre_use_selected_script, {
+      if (req(input$modcre_selected_script) != "No Script yet" && grepl(".R", req(input$modcre_selected_script))) {
+        source(fs::path(models_folder_path(), input$modcre_selected_script))
+        return(result)
+      }
+    })
+    
+    # Affichage // Test de récupérer le résultat d'un script
+    output$modcre_test <- renderPrint({
+      req(input$modcre_use_selected_script)
+      if (!grepl(".R", isolate(req(input$modcre_selected_script)))) {
+        "Nothing happened"
+      } else {
+        test()
+      }
+    })
+    
+# Test Classifier Server --------------------------------------------------
+    
+    
+    
+# Visualise Classifier Server ---------------------------------------------
+    
+    
+    
   })
 }
     
