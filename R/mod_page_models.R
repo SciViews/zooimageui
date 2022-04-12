fd49a23cc5beabf72cba76cb501657f047dd2137
MartinLecocq <Martinl@hotmail.be>
2022-04-12 14:11:38 +0000

    Notes. Mise en place du chargement du modèle, des tests de celui-ci, d'un retour de message si ça fonctionne ou non, d'un commentaire lié au modèle, de la création d'un classifieur si modèle et ts correctes, de l'affichage du classifieur. Résolution de différents problèmes liés à cette mise en place. (par exemple la fonction get_classif se supprime bien avant d'être récupérée). Ajout d'un grand nombre de commentaires.

--- R/mod_page_models.R
+++ R/mod_page_models.R
@@ -21,7 +21,8 @@ mod_page_models_ui <- function(id){
           sidebarPanel(
             h4("Model :"),
             selectInput(ns("modcre_selected_script"), NULL, choices = NULL),
-            verbatimTextOutput(ns("test_test")),
+            verbatimTextOutput(ns("modcre_mod_message")),
+            verbatimTextOutput(ns("modcre_mod_comment")),
             h4("Traing Set :"),
             textOutput(ns("modcre_selected_ts")),
             tags$br(),
@@ -80,6 +81,9 @@ mod_page_models_server <- function(id, all_vars){
     
 # Variables Globales ------------------------------------------------------
     
+    # Variable : timer pour enclencher de la réactivité
+    timer <- reactiveTimer(2000)
+    
     # Variable : Chemin vers le dossier des TS !! Peut être pas nécessaire
     ts_folder_path <- reactive({
       fs::path(data_folder_path_rea(),"Traing_Sets")
@@ -120,27 +124,79 @@ mod_page_models_server <- function(id, all_vars){
       paste(ts_name(), test)
     })
     
-    test <- reactive({
-      if (req(input$modcre_selected_script) != "No Model yet" &&
-          grepl(".R", input$modcre_selected_script)) {
-        source(fs::path(models_folder_path(), input$modcre_selected_script), local = TRUE)
-        res <- exists("bonjour")
-        rm(bonjour)
+    # Test 2 ! d'éxécution du script (modèle) choisi (renvoie vrai ou faux)
+    modcre_is_mod_correct <- reactive({
+      req(input$modcre_selected_script, data_folder_path_rea())
+      
+      # Tout d'abord, test du nom du fichier : si ne ressemble pas à un script R, on arrête avec message d'erreur
+      if ( input$modcre_selected_script == "No Model yet" || !grepl(".R", input$modcre_selected_script)) {
+        res <- FALSE
+        attr(res, "message") <- "Selected Model doesn't have .R extension"
         return(res)
+      } else {
+      # Ensuite, si jamais le nom est bon :
+      
+        # Supprime la possible ancienne fonction et source le nouveau script
+        if (exists("get_classif")) { rm(get_classif) }
+        script_path <- fs::path(models_folder_path(), input$modcre_selected_script)
+        try(source(script_path , local = TRUE), silent = TRUE)
+        
+        # Test si une fonction get_classif est récupérée
+        if (!exists("get_classif")) {
+          res <- FALSE
+          attr(res, "message") <- "Selected Model doesn't return a function called get_classif"
+          return(res)
+        # Test si la fonction a un argument training_set
+        } else if (any(!"training_set" %in% names(formals(get_classif)))) {
+          res <- FALSE
+          attr(res, "message") <- "Selected Model doesn't have a training_set argument"
+          return(res)
+        # Enfin, si ça passe, alors le modèle est bon
+        } else {
+          res <- exists("get_classif")
+          attr(res, "message") <- "Model : Ok"
+          return(res)
+        }
+        
       }
     })
-    output$test_test <- renderPrint({
-      test()
+    
+    # Affichage // Message lié au modèle choisi
+    output$modcre_mod_message <- renderText({
+      # modcre_is_mod_correct()
+      # Récupère le message qui explique si le modèle est bon ou non
+      attr(modcre_is_mod_correct(), "message")
+    })
+    
+    # Affichage // Commentaire lié au modèle choisi
+    output$modcre_mod_comment <- renderText({
+      comment(modcre_model())
+    })
+    
+    # Variable : fonction du modèle
+    modcre_model <- reactive({
+      if (req(modcre_is_mod_correct())) {
+        # Chemin du script
+        script_path <- fs::path(models_folder_path(), input$modcre_selected_script)
+        # Source du script pour récupérer la fonction
+        source(script_path , local = TRUE)
+        return(get_classif)
+      }
     })
     
     # Mise à jour du bouton pour utiliser un script models
     observe({
       shinyjs::disable("modcre_use_selected_script")
-      # Si script choisi, script est .R, TS chargé, alors bouton actif
-      if (req(input$modcre_selected_script) != "No Model yet" && 
-          grepl(".R", req(input$modcre_selected_script)) && modcre_is_ts_loaded()) {
+      # Si le modèle est correcte et que le TS est chargé alors on peut créer le classifieur
+      if (modcre_is_mod_correct() && modcre_is_ts_loaded()) {
         shinyjs::enable("modcre_use_selected_script")
       }
+    })
+    
+    # Variable : Classifieur si appui sur le bouton
+    modcre_classif <- eventReactive(input$modcre_use_selected_script, {
+      # Crée le classifieur
+      return(modcre_model()(ts_training_set()))
     })
     
     # Affichage // Liste des scipts dans le dossier Models
@@ -151,24 +207,6 @@ mod_page_models_server <- function(id, all_vars){
       # Si pas :
       } else {
         "No Model yet"
-      }
-    })
-    
-    # Variable : Si on utilise le script, retourne la variable result de ce dernier
-    modcre_classif <- eventReactive(input$modcre_use_selected_script, {
-      
-      # Variable qui prend le training set chargé pour le script
-      training_set <- ts_training_set()
-      
-      # Il faut un local = TRUE au source afin que l'environnement utilisé soit celui de l'app
-      source(fs::path(models_folder_path(), input$modcre_selected_script), local = TRUE)
-      
-      # Si la variable classif existe bien, on la renvoie
-      if (exists("classif")) {
-        attr(classif, "is_mlRforest") <- is_mlRforest
-        return(classif)
-      } else {
-        return(NULL)
       }
     })
     
